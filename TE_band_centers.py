252ef6bc40e71efba6d13c9de64548be5f280b93
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-05 16:34:00 -0800

    start of TE band center code

---
+++ TE_band_centers.py
@@ -0,0 +1,13 @@
+from pymatgen import Composition, Element
+from pymatgen.matproj.rest import MPRester
+
+__author__ = 'Saurabh Bajaj'
+# __copyright__ = 'Copyright 2014, The Materials Project'
+__version__ = '0.1'
+__maintainer__ = 'Saurabh Bajaj'
+__email__ = 'sbajaj@lbl.gov'
+__date__ = 'Feb 05, 2016'
+
+
+if __name__ == '__main__':
+    mpr = MPRester()
