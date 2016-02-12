75eed7097e91f5ff8b60cd5b9474beeaaef3a4a5
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-12 15:56:27 -0800

    Get band centers for each TE compound

--- TE_band_centers.py
+++ TE_band_centers.py
@@ -11,20 +11,34 @@ __date__ = 'Feb 05, 2016'
 
 
 def get_band_center(form):
-    c = Composition(str(form))
+    comp = Composition(str(form))
     prod = 1.0
-    for el, amt in c.get_el_amt_dict().iteritems():
+    for el, amt in comp.get_el_amt_dict().iteritems():
         prod = prod * (Element(el).X ** amt)
-    return -prod ** (1 / sum(c.get_el_amt_dict().values()))
-
+        return -prod ** (1 / sum(comp.get_el_amt_dict().values()))
 
 if __name__ == '__main__':
     mpr = MPRester()
     f = open('Best_n_p_TEs.csv')
     csv_file = csv.reader(f)
-    csv_file.next()   # Skip header
+    csv_file.next()  # Skip header
     for row in csv_file:
         for compound in row:
-            print Composition(compound)
-
+            c = Composition(compound)
+            center = get_band_center(c)
+            comp_results = mpr.query(c.reduced_formula,
+                                     properties=['band_gap', 'material_id', 'icsd_id', 'e_above_hull'])
+            try:
+                i = comp_results[0]
+                min_e_above_hull = i['e_above_hull']
+            except:
+                print 'No compounds exists in MP for ' + c.reduced_formula
+                break
+            for x in range(1, len(comp_results)):
+                if comp_results[x]['e_above_hull'] < min_e_above_hull:
+                    i = comp_results[x]
+                    min_e_above_hull = comp_results[x]['e_above_hull']
+            print ','.join([str(x) for x in
+                            c.formula, c.reduced_formula, center, center + i['band_gap'] / 2, center - i[
+                                'band_gap'] / 2, i['material_id'], i['icsd_id']])
 
