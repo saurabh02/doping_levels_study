e8e2c4876573b1b8afaa6b72effe9dda2da21e88
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-22 10:25:12 -0800

    dealing with nan(null) values

--- TE_band_centers.py
+++ TE_band_centers.py
@@ -39,25 +39,26 @@ if __name__ == '__main__':
         elif column == 'n-type':
             te_type = 'n-type'
         for compound in df[column].tolist()[1:]:
-            c = Composition(compound)
-            center = get_band_center(c)
-            comp_results = mpr.query(c.reduced_formula,
-                                     properties=['band_gap', 'material_id', 'icsd_id', 'e_above_hull'])
-            try:
-                i = comp_results[0]
-                min_e_above_hull = i['e_above_hull']
-            except:
-                y += 1
-                print 'No compounds exists in MP for ' + c.reduced_formula
-                continue
-            for x in range(1, len(comp_results)):
-                if comp_results[x]['e_above_hull'] < min_e_above_hull:
-                    i = comp_results[x]
-                    min_e_above_hull = comp_results[x]['e_above_hull']
-            print ','.join([str(x) for x in
-                            c.formula, c.reduced_formula, center + i['band_gap'] / 2, center, center, center - i[
-                                'band_gap'] / 2, i['material_id'], i['icsd_id'], column])
-            csv_outfile.writerow([c.reduced_formula, i['band_gap'], i['band_gap'] / 2, center + i['band_gap'] / 2, center, center - i['band_gap'] / 2, i['material_id'], i['icsd_id'], column])
-    print 'Total number of compounds with no data found for in MP = {}'.format(x)
+            if not pandas.isnull(compound):
+                c = Composition(compound)
+                center = get_band_center(c)
+                comp_results = mpr.query(c.reduced_formula,
+                                         properties=['band_gap', 'material_id', 'icsd_id', 'e_above_hull'])
+                try:
+                    i = comp_results[0]
+                    min_e_above_hull = i['e_above_hull']
+                except:
+                    y += 1
+                    print 'No compounds exists in MP for ' + c.reduced_formula
+                    continue
+                for x in range(1, len(comp_results)):
+                    if comp_results[x]['e_above_hull'] < min_e_above_hull:
+                        i = comp_results[x]
+                        min_e_above_hull = comp_results[x]['e_above_hull']
+                print ','.join([str(x) for x in
+                                c.formula, c.reduced_formula, center + i['band_gap'] / 2, center, center, center - i[
+                                    'band_gap'] / 2, i['material_id'], i['icsd_id'], column])
+                csv_outfile.writerow([c.reduced_formula, i['band_gap'], i['band_gap'] / 2, center + i['band_gap'] / 2, center, center - i['band_gap'] / 2, i['material_id'], i['icsd_id'], column])
+    print 'DONE! Total number of compounds with no data found for in MP = {}'.format(y)
     f.close()
     f_w.close()
