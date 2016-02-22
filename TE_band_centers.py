b66de35bd83309bc199f6ac8971f5890a7fa97d7
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-22 10:28:24 -0800

    code cleanup

--- TE_band_centers.py
+++ TE_band_centers.py
@@ -21,18 +21,14 @@ def get_band_center(form):
 
 if __name__ == '__main__':
     mpr = MPRester()
-    f = open('Best_n_p_TEs.csv')
-    csv_file = csv.reader(f)
-    csv_file.next()  # Skip header
     f_w = open('Best_n_p_TEs_out.csv', 'wb')
     csv_outfile = csv.writer(f_w)
-    csv_outfile.writerow(['formula', 'band gap', '(band gap)/2', 'CBM', 'band center', 'VBM', 'materials_id', 'icsd_id'])
+    csv_outfile.writerow(['formula', 'band gap', '(band gap)/2', 'CBM', 'band center', 'VBM', 'materials_id', 'icsd_id',
+                          'Umutlist_type'])
     colnames = ['p-type', 'n-type']
     df = pandas.read_csv('Best_n_p_TEs.csv', names=colnames)
     input_data_ptype = df['p-type'].tolist()[1:]
-    # for row in csv_file:
-    #     for compound in row:
-    y = 0   ## Number of compounds with no MP data
+    y = 0
     for column in df:
         if column == 'p-type':
             te_type = 'p-type'
@@ -58,7 +54,8 @@ if __name__ == '__main__':
                 print ','.join([str(x) for x in
                                 c.formula, c.reduced_formula, center + i['band_gap'] / 2, center, center, center - i[
                                     'band_gap'] / 2, i['material_id'], i['icsd_id'], column])
-                csv_outfile.writerow([c.reduced_formula, i['band_gap'], i['band_gap'] / 2, center + i['band_gap'] / 2, center, center - i['band_gap'] / 2, i['material_id'], i['icsd_id'], column])
+                csv_outfile.writerow(
+                    [c.reduced_formula, i['band_gap'], i['band_gap'] / 2, center + i['band_gap'] / 2, center,
+                     center - i['band_gap'] / 2, i['material_id'], i['icsd_id'], column])
     print 'DONE! Total number of compounds with no data found for in MP = {}'.format(y)
-    f.close()
     f_w.close()
