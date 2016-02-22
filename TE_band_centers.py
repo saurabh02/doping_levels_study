f15f7b188fb0a1c96446de2f0bf6c427198495a5
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-22 10:17:14 -0800

    get all data using Pandas

--- TE_band_centers.py
+++ TE_band_centers.py
@@ -28,11 +28,17 @@ if __name__ == '__main__':
     csv_outfile = csv.writer(f_w)
     csv_outfile.writerow(['formula', 'band gap', '(band gap)/2', 'CBM', 'band center', 'VBM', 'materials_id', 'icsd_id'])
     colnames = ['p-type', 'n-type']
-    input_data = pandas.read_csv('Best_n_p_TEs.csv', names=colnames)
-    input_data_ptype = input_data['p-type'].tolist()[1:]
-    for compound in input_data_ptype:
+    df = pandas.read_csv('Best_n_p_TEs.csv', names=colnames)
+    input_data_ptype = df['p-type'].tolist()[1:]
     # for row in csv_file:
     #     for compound in row:
+    y = 0   ## Number of compounds with no MP data
+    for column in df:
+        if column == 'p-type':
+            te_type = 'p-type'
+        elif column == 'n-type':
+            te_type = 'n-type'
+        for compound in df[column].tolist()[1:]:
             c = Composition(compound)
             center = get_band_center(c)
             comp_results = mpr.query(c.reduced_formula,
@@ -41,6 +47,7 @@ if __name__ == '__main__':
                 i = comp_results[0]
                 min_e_above_hull = i['e_above_hull']
             except:
+                y += 1
                 print 'No compounds exists in MP for ' + c.reduced_formula
                 continue
             for x in range(1, len(comp_results)):
@@ -49,7 +56,8 @@ if __name__ == '__main__':
                     min_e_above_hull = comp_results[x]['e_above_hull']
             print ','.join([str(x) for x in
                             c.formula, c.reduced_formula, center + i['band_gap'] / 2, center, center, center - i[
-                                'band_gap'] / 2, i['material_id'], i['icsd_id']])
-            csv_outfile.writerow([c.reduced_formula, i['band_gap'], i['band_gap'] / 2, center + i['band_gap'] / 2, center, center - i['band_gap'] / 2, i['material_id'], i['icsd_id']])
+                                'band_gap'] / 2, i['material_id'], i['icsd_id'], column])
+            csv_outfile.writerow([c.reduced_formula, i['band_gap'], i['band_gap'] / 2, center + i['band_gap'] / 2, center, center - i['band_gap'] / 2, i['material_id'], i['icsd_id'], column])
+    print 'Total number of compounds with no data found for in MP = {}'.format(x)
     f.close()
     f_w.close()
